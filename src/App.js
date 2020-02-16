71ae301f3b815844d292a62538255b93148ea67f
David González <braben@gmail.com>
2020-02-16 14:23:27 +0000

    App invaded

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { createSchema, Autoform } from 'react-hook-form-auto'
+
+const thing = createSchema('thing', {
+  name: {
+    type: 'string',
+    required: true
+  },
+  mass: {
+    type: 'number'
+  },
+  type: {
+    type: 'select',
+    options: ['solid', 'liquid', 'gas']
+  }
+});
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Autoform
+          schema={thing}
+        />
       </header>
     </div>
   );
